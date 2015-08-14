8fb9ae5c870a6e1160970bccb6b0347272b02f81
RonCoder <ron_benporat@yahoo.com>
2015-08-14 23:30:26 +0000

    added profiles form and create action (stripe issue unresolved)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,22 @@ class ProfilesController < ApplicationController
    def new
      # form where a user can fill out their own profile
      @user = User.find(params[:user_id]) #reach into the database and find the user that's loged in
-     @profile = @user.build_profile
+     @profile = @user.build_profile #builds profile
    end
+   
+   def create
+     @user = User.find(params[:user_id])
+     @profile = @user.build_profile(profile_params) #saves info
+     if @profile.save
+       flash[:success] = "Profile Updated!"
+       redirect_to user_path(params[:user_id])
+     else
+       render action: :new
+     end
+   end
+   
+   private
+    def profile_params
+      params.require(:params).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
