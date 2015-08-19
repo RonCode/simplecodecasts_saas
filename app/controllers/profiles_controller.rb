a228eb5c4cdd39c27a4a19878fba376fe979781d
RonCoder <ron_benporat@yahoo.com>
2015-08-19 03:28:20 +0000

    added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,28 +1,47 @@
 class ProfilesController < ApplicationController
-   def new
-     # form where a user can fill out their own profile
-     @user = User.find(params[:user_id]) #reach into the database and find the user that's loged in
-     @profile = Profile.new
-   end
-   
-   def create
-     @user = User.find(params[:user_id])
-     @profile = @user.build_profile(profile_params) #saves info
-     if @profile.save
-       flash[:success] = "Profile Updated!"
-       redirect_to user_path(params[:user_id])
-     else
-       render action: :new
-     end
-   end
-   
-   def edit
+  before_action :authenticate_user! #before any action below happens, you must be authenticated and signed up to access pages
+  before_action :only_current_user #created method to only allow current user to edit his/her own profile
+  
+  def new
+    # form where a user can fill out their own profile
+    @user = User.find(params[:user_id]) #reach into the database and find the user that's loged in
+    @profile = Profile.new
+  end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params) #saves info
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path(params[:user_id])
+    else
+      render action: :new
+    end
+  end
+  
+  def edit
     @user = User.find(params[:user_id])  
     @profile = @user.profile
-   end
-   
-   private
+  end
+  
+  def update
+    @user = User.find(params[:user_id])    
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path(params[:user_id])
+    else
+      render action: :edit  
+    end
+  end
+  
+  private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find(params[:user_id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user! #stops users who aren't signed in from viewing profiles
+  
   def show
     @user = User.find(params[:id])
   end
