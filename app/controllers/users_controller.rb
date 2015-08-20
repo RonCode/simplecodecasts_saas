3d70f10a7f1ae285da5520b2a51f89be8701319a
RonCoder <ron_benporat@yahoo.com>
2015-08-20 06:08:54 +0000

    improved users index page (stripe issue unresolved)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user! #stops users who aren't signed in from viewing profiles
   
   def index
-    
+    @users = User.all #grabs all registered users
   end
   
   def show
