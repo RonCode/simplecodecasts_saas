2e032f52384c60ebc59d4866f28af1fac64af4a2
RonCoder <ron_benporat@yahoo.com>
2015-08-20 00:00:03 +0000

    added index action and updated links on home page (stripe issue unresolved)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user! #stops users who aren't signed in from viewing profiles
   
+  def index
+    
+  end
+  
   def show
     @user = User.find(params[:id])
   end
